fd6c80003bb98a2a16e5a37fc088be36b7e7579b
novelyaasis32 <novelya32@gmail.com>
2022-05-09 18:44:01 +0700

    file masuk pd link

---
+++ database/migrations/2022_05_09_182025_create_materi_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMateriTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('materi', function (Blueprint $table) {
+            $table->id();
+            $table->string('mapel');
+            $table->string('kelas');
+            $table->string('judul');
+            $table->longText('isi');
+            $table->string('kesimpulan')->nullable();
+            $table->string('keterangan')->nullable();
+            $table->bigInteger('user_id_teacher')->unsigned();
+            $table->string('link');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('materi');
+    }
+}
