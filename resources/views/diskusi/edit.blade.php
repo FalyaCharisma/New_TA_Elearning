54a26cb381b1f676c96a75442c0bf4c6bfcac1a5
= <falyacharisma9@gmail.com>
2022-06-02 14:07:56 +0700

    Diskusi edit dan hapus

--- resources/views/diskusi/edit.blade.php
+++ resources/views/diskusi/edit.blade.php
@@ -4,35 +4,32 @@
 <div class="main-content">
     <section class="section">
         <div class="section-header">
-            <h1>Ajukan Pertanyaan</h1>
+            <h1>Edit Pertanyaan</h1>
         </div>
 
         <div class="section-body">
 
             <div class="card">
                 <div class="card-header">
-                    <h4><i class="fas fa-unlock"></i> Ajukan Pertanyaan</h4>
+                    <h4><i class="fas fa-question"></i> Edit Pertanyaan</h4>
                 </div>
 
                 <div class="card-body">
-                    <form action="{{ route('diskusi.store') }}" method="POST" enctype="multipart/form-data">
+                    <form method="POST" action="{{ url('diskusi', $diskusi->id ) }}"
+                        enctype="multipart/form-data">
                         @csrf
+                        @method('PUT')
                         <div class="form-group">
                             <label>MATERI</label>
                             <select class="form-control select-materi @error('materi') is-invalid @enderror" name="materi">
                                 <option value="">- SELECT MATERI -</option>
-                                @foreach ($materi as $materi) 
-                                @hasrole('student')
+                                @foreach ($materi as $materi)
                                 @if ($materi->siswa ==  Auth::user()->siswa->name)
-                                    <option value="{{ $materi->judul }}">{{ $materi->judul }}</option>
-                                @endif
-                                <!-- @endhasrole
-                                @hasrole('teacher')
-                                @if ($materi->user)id ==  Auth::user()->id)
-                                    <option value="{{ $materi->judul }}">{{ $materi->judul }}</option>
-                                @endhasrole
-                                @endif -->
-                                @endforeach  
+                                    <option value="{{ $materi->judul }}" selected>{{ $materi->judul }}</option>
+                                    @else
+                                        <option value="{{ $materi->judul }}">{{ $materi->judul }}</option>
+                                    @endif
+                                @endforeach
                             </select>
                             @error('materi')
                             <div class="invalid-feedback" style="display: block">
@@ -42,8 +39,9 @@
                         </div>
                         <div class="form-group">
                             <label>ISI PERTANYAAN</label>
-                            <textarea name="pertanyaan" value="{{ old('pertanyaan') }}" class="form-control @error('pertanyaan') is-invalid @enderror" placeholder="Place some text here"
-                            style="width: 100%; height: 200px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;"></textarea>
+
+                            <textarea name="pertanyaan" value="{{ old('pertanyaan', $diskusi->pertanyaan) }}" placeholder="Place some text here" class="form-control @error('pertanyaan') is-invalid @enderror" 
+                            style="width: 100%; height: 200px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;">{{ $diskusi->pertanyaan}}</textarea>
 
                             @error('pertanyaan')
                             <div class="invalid-feedback" style="display: block">
@@ -53,16 +51,13 @@
                         </div>
 
                         <button class="btn btn-primary mr-1 btn-submit" type="submit"><i class="fa fa-paper-plane"></i>
-                            SIMPAN</button>
+                            UPDATE</button>
                         <button class="btn btn-warning btn-reset" type="reset"><i class="fa fa-redo"></i> RESET</button>
 
                     </form>
-
-                    
                 </div>
             </div>
         </div>
     </section>
 </div>
-
 @stop
--- resources/views/diskusi/showDiskusi.blade.php
+++ resources/views/diskusi/showDiskusi.blade.php
@@ -11,28 +11,21 @@
 
                 <div class="card">
                     <div class="card-header">
-                        <h4><i class="fas fa-question"></i> Forum Diskusi</h4>
+                        <h4><i class="fas fa-question"></i> {{ $diskusi->materi }}</h4>
                     </div>
-
 
                     <div class="card-body">
                         @can('diskusi.showDiskusi')
                             <div class="card card-default  mb-2">
-                                <div class="card-header">
-                                    <h5 class="">{{ $diskusi->materi }}</h5>
-                                </div>
                                 <div class="card-body">
                                     <span class="">{{ $diskusi->user->username }},
                                         <b>{{ $diskusi->created_at }}</b></span>
                                     <hr>
-                                    <p class="">{{ $diskusi->pertanyaan }} </p>
+                                    <p class=""><b>Pertanyaan: </b> {{ $diskusi->pertanyaan }} </p>
                                 </div>
-                                <!-- <div class="card-footer">
-                                    <p> Replies</p>
-                                </div> -->
                             </div>
                         @endcan
-
+                     
                         @foreach ($respon as $item)
                             @can('diskusi.showDiskusi')
                                 <div class="card card-default  mb-2">
@@ -42,9 +35,16 @@
                                     </div>
                                     <div class="card-body">
                                         <p class="">{{ $item->respon }} </p>
+                                        <a href="{{ route('diskusi.edit', $diskusi->id) }}" class="btn btn-sm btn-primary">Edit</a>
+                                        <button onClick="Delete(this.id)" class="btn btn-sm btn-danger" style="margin-left: 5px;" id="{{ $item->id }}">
+                                             Hapus
+                                        </button>
                                     </div>
-                                    <div class="card-footer">
-                                    @can('diskusi.respon')
+                                </div>
+                            @endcan
+                        @endforeach
+
+                        @can('diskusi.respon')
                                 <form action="{{ url('diskusi/respon', $diskusi->id) }}" method="POST"
                                     enctype="multipart/form-data">
                                     @csrf
@@ -66,13 +66,71 @@
                                     </div>
                                 </form>
                             @endcan
-                                    </div>
-                                </div>
-                            @endcan
-                        @endforeach
                     </div>
                 </div>
             </div>
         </section>
     </div>
+    <script>
+    //ajax delete
+    function Delete(id)
+        {
+            var id = id;
+            var token = $("meta[name='csrf-token']").attr("content");
+
+            swal({
+                title: "APAKAH KAMU YAKIN ?",
+                text: "INGIN MENGHAPUS DATA INI!",
+                icon: "warning",
+                buttons: [
+                    'TIDAK',
+                    'YA'
+                ],
+                dangerMode: true,
+            }).then(function(isConfirm) { 
+                if (isConfirm) {
+
+                    //ajax delete
+                    jQuery.ajax({
+                        url: "{{ route("diskusi.index") }}/"+id,
+                        data:   {
+                            "id": id,
+                            "_token": token
+                        },
+                        type: 'DELETE',
+                        success: function (response) {
+                            if (response.status == "success") {
+                                swal({
+                                    title: 'BERHASIL!',
+                                    text: 'DATA BERHASIL DIHAPUS!',
+                                    icon: 'success',
+                                    timer: 1000,
+                                    showConfirmButton: false,
+                                    showCancelButton: false,
+                                    buttons: false,
+                                }).then(function() {
+                                    location.reload();
+                                });
+                            }else{
+                                swal({
+                                    title: 'GAGAL!',
+                                    text: 'DATA GAGAL DIHAPUS!',
+                                    icon: 'error',
+                                    timer: 1000,
+                                    showConfirmButton: false,
+                                    showCancelButton: false,
+                                    buttons: false,
+                                }).then(function() {
+                                    location.reload();
+                                });
+                            }
+                        }
+                    });
+
+                } else {
+                    return true;
+                }
+            })
+        }
+</script>
 @stop
