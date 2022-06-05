a38f1a631ee7d1f4955114e896fec4b8cfbf0b75
= <falyacharisma9@gmail.com>
2022-06-06 06:52:31 +0700

    edit profil tentor dan siswa

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -5,10 +5,12 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User; 
+use App\Models\Siswa; 
+use App\Models\Tentor; 
 class ProfileController extends Controller
 {
     public function __construct(){
-        $this->middleware(['permission:profile.index|profile.create|profile.delete|profile.edit']);
+        $this->middleware(['permission:profile.index|profile.create|profile.delete|profile.edit|profile.editTentor']);
     }
 
     public function index(){
@@ -45,6 +47,26 @@ class ProfileController extends Controller
         }
         $user->update();
 
+        return redirect()->route('dashboard.index')->with(['success' => 'Data Berhasil Diupdate!']);
+    }
+
+    public function editSiswa(Request $request){ 
+        $user = User::where('id',Auth::user()->id)->first();
+        $siswa = Siswa::where('user_id', $id)->get();
+        return view('profile.editSiswa', compact('user','siswa'));
+    }
+    public function siswaUpdate(Request $request, $id){ 
+        $siswa = Siswa::find($id)->update($request->all());
+        return redirect()->route('dashboard.index')->with(['success' => 'Data Berhasil Diupdate!']);
+    }
+
+    public function editTentor(Request $request){ 
+        $user = User::where('id',Auth::user()->id)->first();
+        $tentor = Tentor::where('user_id', $id)->get();
+        return view('profile.editTentor', compact('user','tentor'));
+    }
+    public function tentorUpdate(Request $request, $id){ 
+        $tentor = Tentor::find($id)->update($request->all());
         return redirect()->route('dashboard.index')->with(['success' => 'Data Berhasil Diupdate!']);
     }
 
--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -11,26 +11,13 @@
 
             <div class="card">
                 <div class="card-header">
-                    <h4><i class="fas fa-user"></i> Edit Profile</h4>
+                    <h4><i class="fas fa-user"></i> Edit Username & Password</h4>
                 </div>
                 <div class="card-body">
                     <form action="{{ route('profile.update', $user->id) }}" method="POST"
                         enctype="multipart/form-data">
                         @csrf
                         @method('PUT')
-                        <!-- <div class="form-group">
-                            <label>NAMA USER</label>
-                            <input type="text" name="name" value="{{ old('name', $user->name) }}"
-                                placeholder="Masukkan Nama User"
-                                class="form-control @error('name') is-invalid @enderror">
-
-                            @error('name')
-                            <div class="invalid-feedback" style="display: block">
-                                {{ $message }}
-                            </div>
-                            @enderror
-                        </div> -->
-
                         <div class="form-group">
                             <label>USERNAME</label>
                             <input type="text" name="username" value="{{ old('username', $user->username) }}"
@@ -55,31 +42,6 @@
                             </div>
                             @enderror
                         </div>
-
-                        <!-- <div class="form-group">
-                            <label>NO. WA</label>
-                            <input type="text" name="no_wa" value="{{ old('no_wa', $user->no_wa) }}" placeholder="Masukkan Nomor WhatsApp"
-                                class="form-control @error('no_wa') is-invalid @enderror">
-
-                            @error('no_wa')
-                            <div class="invalid-feedback" style="display: block">
-                                {{ $message }}
-                            </div>
-                            @enderror
-                        </div>
-
-                        <div class="form-group">
-                            <label>ALAMAT</label>
-                            <input type="text" name="alamat" value="{{ old('alamat', $user->alamat) }}" placeholder="Masukkan Alamat"
-                                class="form-control @error('alamat') is-invalid @enderror">
-
-                            @error('alamat')
-                            <div class="invalid-feedback" style="display: block">
-                                {{ $message }}
-                            </div>
-                            @enderror
-                        </div> -->
-
                         <div class="row">
                             <div class="col-md-6">
                                 <div class="form-group">
@@ -105,8 +67,114 @@
 
                     </form>
                 </div>
+                @can('profile.edit')
+                <div class="card-header">
+                    <h4><i class="fas fa-user"></i> Edit Profile</h4>
+                </div>
+                <div class="card-body">
+                    <form action="{{ url('profile/siswa/siswaUpdate', $user->siswa->id) }}" method="POST"
+                        enctype="multipart/form-data">
+                        @csrf
+                        <div class="form-group">
+                            <label>NAMA SISWA</label>
+                            <input type="text" name="name" value="{{ old('name', $user->siswa->name) }}"
+                                placeholder="Masukkan Nama User"
+                                class="form-control @error('name') is-invalid @enderror">
+
+                            @error('name')
+                            <div class="invalid-feedback" style="display: block">
+                                {{ $message }}
+                            </div>
+                            @enderror
+                        </div>
+
+                        <div class="form-group">
+                            <label>NO. WA</label>
+                            <input type="text" name="no_wa" value="{{ old('no_wa', $user->siswa->no_wa) }}" placeholder="Masukkan Nomor WhatsApp"
+                                class="form-control @error('no_wa') is-invalid @enderror">
+
+                            @error('no_wa')
+                            <div class="invalid-feedback" style="display: block">
+                                {{ $message }}
+                            </div>
+                            @enderror
+                        </div>
+
+                        <div class="form-group">
+                            <label>ALAMAT</label>
+                            <input type="text" name="alamat" value="{{ old('alamat', $user->siswa->alamat) }}" placeholder="Masukkan Alamat"
+                                class="form-control @error('alamat') is-invalid @enderror">
+
+                            @error('alamat')
+                            <div class="invalid-feedback" style="display: block">
+                                {{ $message }}
+                            </div>
+                            @enderror
+                        </div>
+
+                        <button class="btn btn-primary mr-1 btn-submit" type="submit"><i class="fa fa-paper-plane"></i>
+                            UPDATE</button>
+                        <button class="btn btn-warning btn-reset" type="reset"><i class="fa fa-redo"></i> RESET</button>
+
+                    </form>
+                </div>
+                @endcan
+                @can('profile.editTentor')
+                <div class="card-header">
+                    <h4><i class="fas fa-user"></i> Edit Profile</h4>
+                </div>
+                <div class="card-body">
+                    <form action="{{ url('profile/tentor/tentorUpdate', $user->tentor->id) }}" method="POST"
+                        enctype="multipart/form-data">
+                        @csrf
+                        <div class="form-group">
+                            <label>NAMA TENTOR</label>
+                            <input type="text" name="name" value="{{ old('name', $user->tentor->name) }}"
+                                placeholder="Masukkan Nama User"
+                                class="form-control @error('name') is-invalid @enderror">
+
+                            @error('name')
+                            <div class="invalid-feedback" style="display: block">
+                                {{ $message }}
+                            </div>
+                            @enderror
+                        </div>
+
+                        <div class="form-group">
+                            <label>NO. WA</label>
+                            <input type="text" name="no_wa" value="{{ old('no_wa', $user->tentor->no_wa) }}" placeholder="Masukkan Nomor WhatsApp"
+                                class="form-control @error('no_wa') is-invalid @enderror">
+
+                            @error('no_wa')
+                            <div class="invalid-feedback" style="display: block">
+                                {{ $message }}
+                            </div>
+                            @enderror
+                        </div>
+
+                        <div class="form-group">
+                            <label>ALAMAT</label>
+                            <input type="text" name="alamat" value="{{ old('alamat', $user->tentor->alamat) }}" placeholder="Masukkan Alamat"
+                                class="form-control @error('alamat') is-invalid @enderror">
+
+                            @error('alamat')
+                            <div class="invalid-feedback" style="display: block">
+                                {{ $message }}
+                            </div>
+                            @enderror
+                        </div>
+
+                        <button class="btn btn-primary mr-1 btn-submit" type="submit"><i class="fa fa-paper-plane"></i>
+                            UPDATE</button>
+                        <button class="btn btn-warning btn-reset" type="reset"><i class="fa fa-redo"></i> RESET</button>
+
+                    </form>
+                </div>
+                @endcan
             </div>
         </div>
     </section>
 </div>
+
+
 @stop
