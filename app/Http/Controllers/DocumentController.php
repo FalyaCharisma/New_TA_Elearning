7d9a81ae3ec71929d5dfc9e32bb6842538cffc91
= <falyacharisma9@gmail.com>
2022-06-19 11:15:40 +0700

    document diakses sesuai auth

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -8,52 +8,37 @@ use Illuminate\Support\Facades\Storage;
 
 class DocumentController extends Controller
 {
-    /**
-     * __construct
-     *
-     * @return void
-     */
+    
     public function __construct()
     {
         $this->middleware(['permission:documents.index|documents.create|documents.delete']);
     }
 
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        $documents = Document::latest()->when(request()->q, function($documents) {
-            $documents = $documents->where('title', 'like', '%'. request()->q . '%');
+        $documents = Document::where('user_id', Auth()->id())->latest()->when(request()->q, function($documents) {
+            $documents = $documents->where('title', 'like', '%'. request()->q . '%'); 
         })->paginate(10);
 
         return view('documents.index', compact('documents'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         $this->validate($request, [
             'title'     => 'required',
-            'document'     => 'required|mimes:doc,docx,pdf',
-            'caption'   => 'required'
+            'document'  => 'required|mimes:doc,docx,pdf,pptx,xlsx',
         ]);
 
         //upload document
         $document = $request->file('document');
-        $document->storeAs('public/documents', $document->hashName());
+        $document->storeAs('public/documents', $document->getClientOriginalName());
 
         $document = Document::create([
             'title'     => $request->input('title'),
-            'link'     => $document->hashName(),
-            'caption'   => $request->input('caption')
+            'link'     => $document->getClientOriginalName(),
+            'caption'   => $request->input('caption'),
+            'user_id'   => Auth()->id(), 
         ]);
 
         if($document){
@@ -65,12 +50,6 @@ class DocumentController extends Controller
         }
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         $document = Document::findOrFail($id);
--- database/migrations/2022_06_19_105616_create_documents_table.php
+++ database/migrations/2022_06_19_105616_create_documents_table.php
@@ -15,6 +15,7 @@ class CreateDocumentsTable extends Migration
     {
         Schema::create('documents', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
             $table->string('title');
             $table->string('caption')->nullable();
             $table->string('link');
